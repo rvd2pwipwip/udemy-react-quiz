f4e724bbfcae9dbe3acdb381bb3fd3240544489c
Herve Desrosiers <hdesrosiers@stingray.com>
2026-08-03 07:56:18 -0400

    correct answer at index 0 bug fix with hasAnswered instead of answer for styling

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,12 +1,14 @@
 const Options = ({ question, dispatch, answer }) => {
+  const hasAnswered = answer !== null;
+
   return (
     <div className="options">
       {question.options.map((option, i) => {
         return (
           <button
-            className={`btn btn-option ${i === answer ? "answer" : ""} ${answer ? (i === question.correctOption ? "correct" : "wrong") : ""}`}
+            className={`btn btn-option ${i === answer ? "answer" : ""} ${hasAnswered ? (i === question.correctOption ? "correct" : "wrong") : ""}`}
             key={option}
-            disabled={answer}
+            disabled={hasAnswered}
             onClick={() => {
               dispatch({ type: "newAnswer", payload: i });
             }}
